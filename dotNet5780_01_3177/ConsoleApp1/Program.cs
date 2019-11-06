8fd52c7c57fbee7fdfb79875dd6c87bf8be5dc90
zufnewfield <57449510+zufnewfield@users.noreply.github.com>
2019-11-06 17:35:00 +0200

    Add files via upload

---
+++ dotNet5780_01_3177/ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0;
+            string[] tafrit = { "Please enter your choise.\npress 1 to book", "press 2 to show bookings", "press 3 to show statistics", "press 4 to exit" };//new string[3];
+            bool[,] arr = new bool[12, 31];            
+            while (p != 4)
+            {
+                for (int i = 0; i < 4; i++)
+                    Console.WriteLine(tafrit[i]);
+                p = int.Parse(Console.ReadLine());
+                switch (p)
+                {
+                    case 1:
+                        {
+                            int a, b, m;
+                            Console.WriteLine("First enter the month's number, then the start date, and then for how many days.");
+                            m = int.Parse(Console.ReadLine());
+                            a = int.Parse(Console.ReadLine());
+                            b = int.Parse(Console.ReadLine());
+                            isA(ref arr, m, a, b);
+                            //Console.WriteLine("{0} {1} {2}", m, a, b);
+                            break;
+                        }
+                    case 2:
+                        {
+                            for(int i=0;i<12;i++)
+                            {
+                                for (int j = 0; j < 31; j++)
+                                {
+                                    if (arr[i, j])
+                                    {
+                                        Console.Write("{0}/{1}-", j+1, i);
+                                        for (; j < 31; j++)
+                                        {
+                                            if (!arr[i, j])
+                                            {
+                                                Console.Write("{0}/{1}\n", j , i);break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            int sum = 0; double prec;
+                            for (int i = 0; i < 12; i++)
+                            {
+                                for (int j = 0; j < 31; j++)
+                                {
+                                    if (arr[i, j] == true)
+                                    {
+                                        sum = sum + 1;
+                                    }
+                                }
+                            }
+                            prec = (sum / 365) * 100;
+                            Console.WriteLine("{0} days out of the 365 days are booked. which is {1} precents booked.", sum, prec);
+                            break;
+                        }
+                }
+            }
+            Console.ReadKey();
+        }
+        static void isA(ref bool[,] arr, int m, int a, int b)
+        {
+            for (int i = a-1; i < a - 1 + b; i++)
+            {
+                if (arr[m, i]==true)
+                {
+                    Console.WriteLine("Request Denied");
+                    return;
+                }
+            }
+            Console.WriteLine("Request accepted");
+            for (int i = a-1; i < a - 1 + b; i++)
+            {
+                arr[m, i] = true;
+            }
+            return;
+        }
+    }
+    
+}
---
+++ dotNet5780_01_3177/dotNet5780_01_3177/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_01_3177
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32[] arr1 = new Int32[20];
+            Int32[] arr2 = new Int32[20];
+            Int32[] arr3 = new Int32[20];
+            Random r = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                arr1[i] = r.Next(18, 122);
+                arr2[i] = r.Next(18, 122);
+                if (arr1[i] > arr2[i])
+                {
+                    arr3[i] = arr1[i] - arr2[i];
+                }
+                else
+                {
+                    arr3[i] = arr2[i] - arr1[i];
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0,4} ", arr1[i]);
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0,4} ", arr2[i]);
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0,4} ", arr3[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
